9b62189f2cf2866394f90564203afe8f866493e3
yashdadhich <98592915+yashdadhich@users.noreply.github.com>
2022-08-19 23:21:31 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var age = prompt("What is your age?");
+
+if (Number(age) < 18) {
+	alert("Sorry, you are too yound to drive this car. Powering off");
+} else if (Number(age) > 18) {
+	alert("Powering On. Enjoy the ride!");
+} else if (Number(age) === 18) {
+	alert("Congratulations on your first year of driving. Enjoy the ride!");
+}
+//age = function checkdriverage() {
+  // console.log("age");
+   //Return:"age";
+
+    
+}
+//checkdriverage();
+//checkdriverage();
+function Meranaam(a,b){
+
+    return a+b;
+
+}
+alert= Meranaam();
